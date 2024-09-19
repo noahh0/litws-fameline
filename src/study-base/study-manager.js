c740562af41a9e19e7d7582643d474b893154e02
Nigini Oliveira <dev+github@nigini.me>
2024-09-19 13:50:50 -0700

    Integrate improvements from the study-template (#9)
    * Remove css class that was breaking the Introduction slide formatting.
    
    * Update LICENSE.txt
    
    * Update jspsych-display-slide.js
    
    Changed tracking info to use `slide.name` instead of `html.id`
    
    * Many small improvements both around code housekeeping and documentation changes towards V2 final.
    
    * Bumped bootstrap version up.
    
    * Added local storage to the demographics form (issue #20), and another questionnaire template.
    
    * Removed professional school from education options on the demographics form and changed wording and default option in the final comments form.
    
    * Added new questionnaire template, changed the HTML loader, and fixed buttons in a group to have the same size.
    
    * Reintroduced the slides before the questionnaires.
    
    * Revamped the results page footer and started cleaning up the LITW CSS.
    
    * Added new LITW Informed Consent page.
    
    * Fixed small issue on new LITW IRB.
    
    * Fixed the use of variables inside the progress partial when building for deployment.

--- src/study-base/study-manager.js
+++ src/study-base/study-manager.js
@@ -10,25 +10,50 @@
  *************************************************************/
 
 // load webpack modules
-window.$ = window.jQuery = require("jquery");
-window.bootstrap = require("bootstrap");
+window.$ = require("jquery");
+window.jQuery = window.$;
+require("../js/jquery.i18n");
+require("../js/jquery.i18n.messagestore");
 require("jquery-ui-bundle");
-var _ = require('lodash');
-var introTemplate = require("../templates/introduction.html");
-var irbTemplate = require("../templates/irb.html");
-var demographicsTemplate = require("../templates/demographics.html");
-var instructionsTemplate = require("../templates/instructions.html");
-var loadingTemplate = require("../templates/loading.html");
-var resultsTemplate = require("../templates/results.html");
-var resultsFooter = require("../templates/results-footer.html");
-var commentsTemplate = require("../templates/comments.html");
-require("../js/litw/jspsych-display-info");
+let Handlebars = require("handlebars");
+window.$.alpaca = require("alpaca");
+window.bootstrap = require("bootstrap");
+window._ = require("lodash");
+
+//LOAD THE HTML FOR STUDY PAGES
+import progressHTML from "../templates/progress.html";
+Handlebars.registerPartial('prog', Handlebars.compile(progressHTML));
+import introHTML from "../templates/introduction.html";
+import irbHTML from "../templates/irb.html";
+import irb_LITW_HTML from "../templates/irb2-litw.html";
+import questHTML from "../templates/questionnaire.html";
+import demographicsHTML from "../templates/demographics.html";
+import loadingHTML from "../templates/loading.html";
+import resultsHTML from "../templates/results.html";
+import resultsFooterHTML from "../templates/results-footer.html";
+import commentsHTML from "../templates/comments.html";
+
 require("../js/litw/jspsych-display-slide");
+//CONVERT HTML INTO TEMPLATES
+let introTemplate = Handlebars.compile(introHTML);
+let irbTemplate = Handlebars.compile(irbHTML);
+let irbLITWTemplate = Handlebars.compile(irb_LITW_HTML);
+let questTemplate = Handlebars.compile(questHTML);
+let demographicsTemplate = Handlebars.compile(demographicsHTML);
+let loadingTemplate = Handlebars.compile(loadingHTML);
+let resultsTemplate = Handlebars.compile(resultsHTML);
+let resultsFooterTemplate = Handlebars.compile(resultsFooterHTML);
+let commentsTemplate = Handlebars.compile(commentsHTML);
 
 //TODO: document "params.study_id" when updating the docs/7-ManageData!!!
 module.exports = (function(exports) {
-	var timeline = [],
-	params = {
+	const study_times= {
+			SHORT: 5,
+			MEDIUM: 10,
+			LONG: 15,
+		};
+	let timeline = [];
+	let params = {
 		study_id: "TO_BE_ADDED_IF_USING_LITW_INFRA",
 		study_recommendation: [],
 		preLoad: ["../img/btn-next.png","../img/btn-next-active.png","../img/ajax-loader.gif"],
@@ -47,13 +72,41 @@ module.exports = (function(exports) {
 				display_element: $("#irb"),
 				display_next_button: false,
 			},
+			INFORMED_CONSENT_LITW: {
+				name: "informed_consent",
+				type: "display-slide",
+				template: irbLITWTemplate,
+				template_data: {
+					time: study_times.SHORT,
+				},
+				display_element: $("#irb"),
+				display_next_button: false,
+			},
+			QUESTIONNAIRE_1: {
+				name: "quest1",
+				type: "display-slide",
+				template: questTemplate,
+				display_element: $("#quest1"),
+				display_next_button: false,
+			},
+			QUESTIONNAIRE_2: {
+				name: "quest2",
+				type: "display-slide",
+				template: questTemplate,
+				display_element: $("#quest2"),
+				display_next_button: false,
+			},
 			DEMOGRAPHICS: {
 				type: "display-slide",
 				template: demographicsTemplate,
+				template_data: {
+					local_data_id: 'LITW_DEMOGRAPHICS'
+				},
 				display_element: $("#demographics"),
 				name: "demographics",
 				finish: function(){
-					var dem_data = $('#demographicsForm').alpaca().getValue();
+					let dem_data = $('#demographicsForm').alpaca().getValue();
+					LITW.data.addToLocal(this.template_data.local_data_id, dem_data);
 					LITW.data.submitDemographics(dem_data);
 				}
 			},
@@ -84,8 +137,67 @@ module.exports = (function(exports) {
 		timeline.push(params.slides.INTRODUCTION);
 		timeline.push(params.slides.INFORMED_CONSENT);
 		timeline.push(params.slides.DEMOGRAPHICS);
+
+		params.slides.QUESTIONNAIRE_1.template_data = getQuest1Data('quest1', 50);
+		timeline.push(params.slides.QUESTIONNAIRE_1);
+		params.slides.QUESTIONNAIRE_2.template_data =
+		getQuest2Data('quest2', './img/cat-computer.png', 100);
+		timeline.push(params.slides.QUESTIONNAIRE_2);
+
+
 		timeline.push(params.slides.COMMENTS);
 		timeline.push(params.slides.RESULTS);
+	}
+
+	function getQuest1Data(quest_id, completion) {
+		return {
+			title: $.i18n(`litw-study-${quest_id}-title`),
+			progress: {
+				value: completion
+			},
+			quest_id: quest_id,
+			done_button: $.i18n(`litw-study-${quest_id}-save`),
+			questions: [1, 2].map((x)=> {
+				return {
+					id: x,
+					text: $.i18n(`litw-study-${quest_id}-q${x}`)
+				}
+			}),
+			responses: [1, 2, 3, 4, 5].map((x)=> {
+				return {
+					id: x,
+					text: $.i18n(`litw-study-quest-a${x}`)
+				}
+			})
+		}
+	}
+
+	function getQuest2Data(quest_id, img_url, completion) {
+		return {
+			title: $.i18n(`litw-study-${quest_id}-title`),
+			img_prompt: {
+				url: img_url,
+				text_before: $.i18n(`litw-study-${quest_id}-prompt`),
+			},
+			progress: {
+				value: completion
+			},
+			quest_id: quest_id,
+			done_button: $.i18n(`litw-study-${quest_id}-save`),
+			questions: [1, 2].map((q)=> {
+				return {
+					id: q,
+					text: $.i18n(`litw-study-${quest_id}-q${q}`),
+				}
+				//ALERT: You can also add responses for each question.
+			}),
+			responses: [1, 2, 3, 4, 5].map((x)=> {
+				return {
+					id: x,
+					text: $.i18n(`litw-study-quest-a${x}`)
+				}
+			})
+		}
 	}
 
 	function calculateResults() {
@@ -105,7 +217,7 @@ module.exports = (function(exports) {
 				data: results
 			}));
 		if(showFooter) {
-			$("#results-footer").html(resultsFooter(
+			$("#results-footer").html(resultsFooterTemplate(
 				{
 					share_url: window.location.href,
 					share_title: $.i18n('litw-irb-header'),
@@ -122,7 +234,7 @@ module.exports = (function(exports) {
 		$.getJSON( "summary.json", function( data ) {
 			//TODO: 'data' contains the produced summary form DB data
 			//      in case the study was loaded using 'index.php'
-			//SAMPLE: The example code gets the cities of study partcipants.
+			//SAMPLE: The example code gets the cities of study particpants.
 			console.log(data);
 		});
 	}
--- src/study-base/webpack.config.js
+++ src/study-base/webpack.config.js
@@ -1,6 +1,9 @@
 var path = require("path");
+const Handlebars = require("handlebars");
 
 var config = {
+  mode: 'development',
+  // mode: 'production',
   entry: path.join(__dirname, "study-manager.js"),
   output: {
     path: path.join(__dirname, "js"),
@@ -9,28 +12,15 @@ var config = {
   module: {
     rules: [
         {
-          test: require.resolve('jquery'),
-            use: [{
-              loader: 'expose-loader',
-              options: {
-                exposes: "jquery",
-              },
-            },
-            {
-              loader: 'expose-loader',
-              options: {
-                exposes: '$',
-              },
-            }
-            ]
-        },
-        {
-          test: /.*\.html$/, loader: "handlebars-loader"
+          test: /.*\.html$/,
+          loader: "html-loader",
+          options: {
+            sources: false,
+          }
         }
     ]
   },
   externals: [
-    /^(jquery.i18n|\$)$/i,
     {
        d3: "d3"
     }
